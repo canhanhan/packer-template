e9bcec198cac27d609811c483db57d09dbb1cb54
Can Hanhan <p_github@finarfin.net>
2018-10-13 12:06:34 -0500

    Initial tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def packer_validate(host, template, args={}):
+    path = "/tmp/packer/"
+    param_list = ['--var "{}={}"'.format(k, v) for k, v in args.items()]
+    params = ' '.join(param_list)
+    cmd = host.run("packer validate {} {}{}".format(params, path, template))
+    assert cmd.rc == 0, "Validation of {} failed. STDOUT: {}\nSTDERR: {}" \
+        .format(template, cmd.stdout, cmd.stderr)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_validate_core_template_for_virtualbox(host):
+    packer_validate(host, "Windows2012R2CoreVirtualbox.json")
+
+
+def test_validate_core_template_for_azure_arm(host):
+    packer_validate(host, "Windows2012R2CoreAzureARM.json", {
+        'client_id': 'sample_value',
+        'client_secret': 'sample_value',
+        'subscription_id': 'sample_value'
+    })
+
+
+def test_validate_core_template_for_amazon_ebs(host):
+    packer_validate(host, "Windows2012R2CoreAmazonEBS.json")
