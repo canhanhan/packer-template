32d2b6b4ea35f0e6fa90cbfcec83f71347076465
Can Hanhan <can.hanhan@hyatt.com>
2018-12-27 08:15:22 -0600

    Support creating external packages

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -19,17 +19,5 @@ def test_validate_custom_template(host):
     packer_validate(host, "CustomBuild.json")
 
 
-def test_validate_core_template_for_virtualbox(host):
-    packer_validate(host, "Windows2012R2CoreVirtualbox.json")
-
-
-def test_validate_core_template_for_azure_arm(host):
-    packer_validate(host, "Windows2012R2CoreAzureARM.json", {
-        'client_id': 'sample_value',
-        'client_secret': 'sample_value',
-        'subscription_id': 'sample_value'
-    })
-
-
-def test_validate_core_template_for_amazon_ebs(host):
-    packer_validate(host, "Windows2012R2CoreAmazonEBS.json")
+def test_validate_package(host):
+    packer_validate(host, "PackageBuild.json")
